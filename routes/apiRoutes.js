66fb4b089f2f725cad936c37e422dbe1374427f9
Matt Leuer <mattleuer@gmail.com>
2020-06-03 15:42:03 -0700

    working CRUD routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
+const mongojs = require("mongojs");
+
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
@@ -10,5 +12,49 @@ router.get("/api/workouts", (req, res) => {
         res.json(err);
       });
   });
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $push: { 
+          exercises: req.body
+        } 
+      },
+  
+      (error, edited) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        } else {
+          console.log(edited);
+          res.send(edited);
+        }
+      }
+    );
+  });
+
 
 module.exports = router;
